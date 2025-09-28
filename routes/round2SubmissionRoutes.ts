22a967bd05d786c1ad0287c44197db2c94574cd1
DARSHAN-V-G <darshanguna22@gmail.com>
2025-09-28 09:11:52 +0530

    .

--- routes/round2SubmissionRoutes.ts
+++ routes/round2SubmissionRoutes.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import { storage } from '../utils/cloudinaryConfig.js';
 import { createRound2Submission, getRound2Submission, checkRound2Submission } from '../controller/round2SubmissionController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { adminProtect } from '../middleware/adminMiddleware.js';
 
 const router = express.Router();
 
@@ -10,8 +10,8 @@ const router = express.Router();
 const upload = multer({ storage });
 
 // Routes
-router.post('/submit', protect, upload.array('images', 10), createRound2Submission); // Allow up to 10 images
-router.get('/checksubmission/:questionId', protect, checkRound2Submission); // Check if submission exists
-router.get('/:questionId', protect, getRound2Submission);
+router.post('/submit',  upload.array('images', 10), createRound2Submission); // Allow up to 10 images
+router.get('/checksubmission/:questionId', checkRound2Submission); // Check if submission exists
+router.get('/:questionId', adminProtect, getRound2Submission);
 
 export default router;
